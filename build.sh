493a236da1f0368bbbca943cbfd30864de0590a4
VISHNU PRIYA  B <4mh23cd061@gmail.com>
2026-06-09 17:36:08 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
-
+pipeline {
+agent any
+stages {
+stage('Clone Repository') {
+steps {
+git branch: 'main',
+url: 
+}
+stage('Status') {
+steps {
+echo "Sucessfully fetched the repo"'https://github.com/your-username/your-repo.git'
+}
+}
+}
